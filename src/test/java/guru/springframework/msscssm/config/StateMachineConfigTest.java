c9914afe663dac35cf2504190224a438cfc5bbff
DodinD1 <62258853+dandodin@users.noreply.github.com>
2020-04-24 09:41:08 -0700

    save changes

---
+++ src/test/java/guru/springframework/msscssm/config/StateMachineConfigTest.java
@@ -0,0 +1,33 @@
+package guru.springframework.msscssm.config;
+
+import guru.springframework.msscssm.domain.PaymentEvent;
+import guru.springframework.msscssm.domain.PaymentState;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.statemachine.StateMachine;
+import org.springframework.statemachine.config.StateMachineFactory;
+
+import java.util.UUID;
+
+@SpringBootTest
+@Slf4j
+class StateMachineConfigTest {
+
+    @Autowired
+    StateMachineFactory<PaymentState, PaymentEvent> factory;
+
+    @Test
+    void testNewStateMachine() {
+        StateMachine<PaymentState, PaymentEvent> sm = factory.getStateMachine(UUID.randomUUID());
+        sm.start();
+        log.info(sm.getState().toString());
+        sm.sendEvent(PaymentEvent.PRE_AUTHORIZE);
+        log.info(sm.getState().toString());
+        sm.sendEvent(PaymentEvent.PRE_AUTH_APPROVED);
+        log.info(sm.getState().toString());
+        sm.sendEvent(PaymentEvent.PRE_AUTH_DECLINED);
+        log.info(sm.getState().toString());
+    }
+}
